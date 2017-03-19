c26e987b6e94b47c0155a8c33b536416e42af63e
Glynn Forrest <me@glynnforrest.com>
2017-03-19 21:26:28 +0000

    mac: tweaks to dock and hot corners

--- salt/mac/setup.sh
+++ salt/mac/setup.sh
@@ -15,6 +15,23 @@ defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+
+# top left starts screensaver
+defaults write com.apple.dock wvous-tl-corner -int 5
+defaults write com.apple.dock wvous-tl-modifier -int 0
+
 # don't save to cloud by default
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
@@ -23,6 +40,28 @@ sudo pmset -a standbydelay 86400
 
 # disable boot sound
 sudo nvram SystemAudioVolume=" "
+
+# auto hide the dock
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+
+# minimize windows into the application's icon
+defaults write com.apple.dock minimize-to-application -bool false
+
+# give minimized apps transparent icons
+defaults write com.apple.dock showhidden -bool true
+
+# reasonable size for dock icons
+defaults write com.apple.dock tilesize -int 46
+
+# restart the tweaked apps
+for app in "cfprefsd" \
+	"Dock" \
+	"Finder" \
+	"SystemUIServer"
+do
+    killall "${app}" &> /dev/null
+done;
 
 # success!
 touch ~/.mac-setup
